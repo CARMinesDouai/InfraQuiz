9924d2545b6ec7564fd9faab26e979f7e1cacccd
maxmario42 <38248353+maxmario42@users.noreply.github.com>
2019-05-07 22:19:47 +0200

    Oups

--- recreateFromBase.sh
+++ recreateFromBase.sh
@@ -4,4 +4,4 @@ rm -rf InfraQuiz.image InfraQuiz.changes
 
 ./pharo ImageBase.image save InfraQuiz
 
-./pharo InfraQuiz.image st --save --quit src/InfraQuiz.stx
+./pharo InfraQuiz.image st --save --quit src/InfraQuiz.st
