79fe9407b6da860aefc143e739f50468452bfecf
maxmario42 <maxence.godefert@gmail.com>
2019-05-15 17:58:59 +0200

    Correct Install

--- installDocker.sh
+++ installDocker.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 apt-get update
-apt-get install apt-transport-https ca-certificates  curl software-properties-common gcc
+apt-get install -y apt-transport-https ca-certificates  curl software-properties-common gcc
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-get install docker-ce
-usermod -aG docker $USER
+apt-get install -y docker-ce
+groupadd docker
+usermod -a -G docker $USER
 systemctl start docker
 systemctl enable docker
 
